075572adb751c66d61ed2b97324ab9e327b32d2d
android-47 <jx.garcia@outlook.com>
2020-10-09 20:42:59 -0700

    App is now able to remain logged in if user closes app completely and re-launches again. Also, if the user logs out and closes the app, when re-launched the app will start off at login menu again to ask for credentials.

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,10 +16,20 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    
+    
     @IBAction func onLoginbutton(_ sender: Any) {
-        print ("test")
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            // Checks if user has logged in previously
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print ("could not login")
