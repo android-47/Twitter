26c7a4c248e1f3bc56efeaf58c242b3f6c4e9016
android-47 <jx.garcia@outlook.com>
2020-10-09 21:41:23 -0700

    Added profile picture image, user name label, and tweet content to table view cell.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -27,16 +27,27 @@ class HomeTableViewController: UITableViewController {
     }
     
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        cell.userNameLabel.text = "some name"
+        cell.tweetContent.text = "something else"
+        
+        return cell
+    }
+    
+    
     // MARK: - Table view data source
 
+    // How many sections do you want?
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    // How many rows of sections do you want?
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
     /*
